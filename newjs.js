c3044966a14f430b3456484bfe004278f7101ae7
tmk789 <kubik.taylor1@hotmail.com>
2024-01-17 12:15:07 -0600

    added function and for loop

--- newjs.js
+++ newjs.js
@@ -1,14 +1,45 @@
-/* filename: newjs.js 
+"use strict"
+/* 
 Author: Taylor Kubik
 Date: 1/17/2024
 
 */
 
-let number = 1;
-let number1 = number + 134;
-
+let number = 230;
+let number1 = number * 134;
 var req = prompt("Please enter something");
 
-console.log("This is what you entered: " + req + " is stored in req");
+
+
+console.log("This is what you entered: " + req + " and is stored in req");
 
 console.log("this is the number: " + number1 + " and is stored in number1");
+
+let newnumber1 = 455;
+
+for(var i = 1; i <= 5; i++){
+    newfunction(newnumber1);
+    newnumber1++;
+}
+
+
+function newfunction(parameter1)
+    {
+        if (parameter1 === 0) {
+            console.log(parameter1 + " is even");
+
+        }
+        
+        else if (parameter1 % 2 === 0) {
+            console.log(parameter1 + " is even");
+        }
+
+        else {
+            console.log(parameter1 + " is odd");
+        }
+        return parameter1;
+    }
+    
+
+
+
