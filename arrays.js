2915f654f7399a9e47bb68cf22269afe2a5f6e4c
tmk789 <kubik.taylor1@hotmail.com>
2024-01-23 14:41:13 -0600

    fixed arrays.js and added a nav to my portfolio site

--- arrays.js
+++ arrays.js
@@ -10,43 +10,43 @@ Date 1/23/2024
 const newArray = ["i", "am", "an", "array"];
 
 
-for(let i in newArray)
-{
-    let num = newFunction(i);
-    
-    
-    twoParameters(num, i);
 
-}
 
-function newFunction(parameter1)
-{
-
+const newFunction = function (parameter1){
     parameter1++;
     
     console.log(parameter1);
-
+    
     return parameter1;
 
+    
 }
 
-function twoParameters(param1, param2)
+
+
+
+
+function newnum(param1, param2)
 {
-    
-    if (param1 === 0) {
-        param2 = param1;
-
+    if (param2 === 0){
+        return newFunction(param2);
     }
+    
+    else if (param2 % 2 === 0) {
+        return newFunction(param2);
 
-    else if (param1 % 2 === 0) {
-        param2 = param1;
     }
 
     else {
-        console.log(param1 + " is odd");
+        console.log(param2 + " is odd");
     }
     
-    return param1, param2;
+    
+}
+
+
+for (let num in newArray){
+    newnum(newFunction, num);
     
 }
 
