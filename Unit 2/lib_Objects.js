dd04bb75ec55e5d035b5bedc13de99616b07c890
tmk789 <kubik.taylor1@hotmail.com>
2024-02-24 09:22:09 -0600

    Unit 2

---
+++ Unit 2/lib_Objects.js
@@ -0,0 +1,16 @@
+//class
+class newClass {
+    //constructor given a name param
+    constructor(name){
+        //this keyword setting name property of the
+        //current instance
+        this.name = name;
+    }
+    //method
+    newName() {  
+    //return to display in console
+        return console.log(this.name);  
+    }
+}
+
+
---
+++ Unit 2/objects.js
@@ -0,0 +1,13 @@
+"use strict"
+//create new objects of the class from the library and then call new name after and log it
+const newName = new newClass("john").newName();
+
+//create new objects of the class from the library and then call new name after and log it
+const newName1 = new newClass("Alfredo").newName();
+
+//create new objects of the class from the library and then call new name after and log it
+const newName2 = new newClass("Taylor").newName();
+
+
+
+
